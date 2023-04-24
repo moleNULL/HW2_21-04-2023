1da923f54847e1453df1720446dbf96811486577
moleNULL <94998782+moleNULL@users.noreply.github.com>
2023-04-24 17:57:34 +0300

    Extracted class in Task_1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -58,47 +58,4 @@ namespace Task_1
             }
         }
     }
-
-    internal class DigitAnalyzer
-    {
-        private string? _text;
-
-        public DigitAnalyzer() { }
-
-        public DigitAnalyzer(string? text)
-        {
-            Text = text;
-        }
-
-        public string? Text
-        {
-            get => _text;
-            set
-            {
-                _text = value;
-                CalculateDigitsSumAndMaxValue();
-            }
-        }
-        public int? MaxDigit { get; private set; }
-        public int? Sum { get; private set; }
-        public bool HasAnyDigits => MaxDigit.HasValue && Sum.HasValue;
-
-        private void CalculateDigitsSumAndMaxValue()
-        {
-            Sum = null;
-            MaxDigit = null;
-
-            if (_text is not null)
-            {
-                IEnumerable<char> charDigits = _text.Where(char.IsDigit);
-
-                if (charDigits.Any())
-                {
-                    Sum = charDigits.Sum(ch => (int)char.GetNumericValue(ch));
-                    MaxDigit = charDigits.Max(ch => (int)char.GetNumericValue(ch));
-                }
-            }
-        }
-
-    }
 }
